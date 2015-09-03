51453552d4105b5ac4bd3ea65f4650cee6acc5f6
avsh <avterus@gmail.com>
2015-09-03 12:21:43 +0300

    refactor Money

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,10 @@
 ## Changelog
 
+### 2.2.1
+
+  * Refactor `Money` to handle amount and cents independently when a
+    currency set
+
 ### 2.2.0
 
   * Rename ``GetPaymentPageToken`` class to ``GetPaymentToken``
--- test/beGateway/MoneyTest.php
+++ test/beGateway/MoneyTest.php
@@ -6,8 +6,8 @@ class MoneyTest extends TestCase {
   public function test_setAmount_with_decimals() {
     $money = $this->getTestObject();
 
-    $money->setCurrency('EUR');
     $money->setAmount(10.57);
+    $money->setCurrency('EUR');
 
     $this->assertEqual($money->getCents(),1057);
     $this->assertEqual($money->getAmount(),10.57);
@@ -16,8 +16,8 @@ class MoneyTest extends TestCase {
   public function test_setAmount_without_decimals() {
     $money = $this->getTestObject();
 
-    $money->setCurrency('BYR');
     $money->setAmount(2550);
+    $money->setCurrency('BYR');
 
     $this->assertEqual($money->getCents(),2550);
     $this->assertEqual($money->getAmount(),2550);
@@ -26,8 +26,8 @@ class MoneyTest extends TestCase {
   public function test_setCents_with_decimals() {
     $money = $this->getTestObject();
 
-    $money->setCurrency('EUR');
     $money->setCents(1057);
+    $money->setCurrency('EUR');
 
     $this->assertEqual($money->getCents(),1057);
     $this->assertEqual($money->getAmount(),10.57);
@@ -36,8 +36,8 @@ class MoneyTest extends TestCase {
   public function test_setCents_without_decimals() {
     $money = $this->getTestObject();
 
-    $money->setCurrency('BYR');
     $money->setCents(2550);
+    $money->setCurrency('BYR');
 
     $this->assertEqual($money->getCents(),2550);
     $this->assertEqual($money->getAmount(),2550);
