f4856927e591cc9f995d8190312e76c5f26f5177
avsh <avterus@gmail.com>
2015-09-21 23:27:35 +0300

    fix test data setup

--- examples/test_shop_data.php
+++ examples/test_shop_data.php
@@ -1,4 +1,4 @@
 <?php
-\beGateway\Settings::setShopId(361);
-\beGateway\Settings::setShopKey('b8647b68898b084b836474ed8d61ffe117c9a01168d867f24953b776ddcb134d');
+\beGateway\Settings::$shopId = 361;
+\beGateway\Settings::$shopKey = 'b8647b68898b084b836474ed8d61ffe117c9a01168d867f24953b776ddcb134d';
 ?>
