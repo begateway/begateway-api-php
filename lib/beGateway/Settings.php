1ec0096c9743dd8e7aef54a7522c6836b3eb86fb
avsh <avterus@gmail.com>
2016-04-09 13:07:48 +0300

    add public key

--- lib/beGateway/Settings.php
+++ lib/beGateway/Settings.php
@@ -5,6 +5,7 @@ namespace beGateway;
 class Settings {
   public static $shopId;
   public static $shopKey;
+  public static $shopPubKey;
   public static $gatewayBase = 'https://demo-gateway.begateway.com';
   public static $checkoutBase = 'https://checkout.begateway.com';
 }
