bdde974cef43f74ae0638aa4df822b682d6fd5f5
avsh <avterus@gmail.com>
2017-10-17 18:33:49 +0300

    update test data file

--- examples/test_shop_data.php
+++ examples/test_shop_data.php
@@ -1,4 +1,7 @@
 <?php
-\beGateway\Settings::$shopId = 361;
-\beGateway\Settings::$shopKey = 'b8647b68898b084b836474ed8d61ffe117c9a01168d867f24953b776ddcb134d';
+\BeGateway\Settings::$shopId = 361;
+\BeGateway\Settings::$shopKey = 'b8647b68898b084b836474ed8d61ffe117c9a01168d867f24953b776ddcb134d';
+\BeGateway\Settings::$checkoutBase = 'https://checkout.begateway.com';
+\BeGateway\Settings::$gatewayBase  = 'https://demo-gateway.begateway.com';
+\BeGateway\Settings::$apiBase      = 'https://api.begateway.com';
 ?>
