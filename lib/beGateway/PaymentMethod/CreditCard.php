a9e174e96edce24dd88941db59ae9aaaae67586b
Alex Shostak <avs@kayako2.ecomcharge.com>
2017-10-18 19:06:11 +0000

    fix file name cases

--- lib/beGateway/PaymentMethod/CreditCard.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace BeGateway\PaymentMethod;
-
-class CreditCard extends Base {
-}
-?>
--- lib/beGateway/PaymentMethod/CreditCardHalva.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace BeGateway\PaymentMethod;
-
-class CreditCardHalva extends Base {
-  public function getName() {
-    return 'halva';
-  }
-}
-?>
