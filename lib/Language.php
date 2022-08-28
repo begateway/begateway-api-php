09e512812f56c9dfd1159eb54a6e2854551d7b69
Alexander <agel63@mail.ru>
2022-08-28 21:26:51 +0300

    fix language code

--- lib/Language.php
+++ lib/Language.php
@@ -1,17 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BeGateway;
 
 class Language
 {
-    public static function getSupportedLanguages()
+    public static function getSupportedLanguages(): array
     {
         return [
-          'en', 'es', 'tr', 'de', 'it', 'ru', 'zh', 'fr', 'da', 'fi', 'no', 'pl', 'sv', 'ja', 'be', 'ka', 'uk',
+            'en',
+            'es',
+            'tr',
+            'de',
+            'it',
+            'ru',
+            'zh',
+            'fr',
+            'da',
+            'fi',
+            'no',
+            'pl',
+            'sv',
+            'ja',
+            'be',
+            'ka',
+            'uk',
+            'lv',
+            'az',
+            'ro',
         ];
     }
 
-    public static function getDefaultLanguage()
+    public static function getDefaultLanguage(): string
     {
         return 'en';
     }
